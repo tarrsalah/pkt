634bb33878ace3ed94cfc2359978064865ebb32d
Salah Eddine Taouririt <tarrsalah@gmail.com>
2021-05-15 10:04:52 +0100

    Don't expose inner types.

--- retrieve.go
+++ retrieve.go
@@ -4,43 +4,11 @@ import (
 	"sort"
 )
 
-type RetrieveOptions struct {
-	DetailType string `json:"detailType"`
-	Since      string `json:"since"`
-	Offset     int    `json:"offset"`
-	Sort       string `json:"sort"`
-	Count      int    `json:"count"`
-}
-
-type RetrieveRequest struct {
-	Auth
-	RetrieveOptions
-}
-
-type RetrieveResponse struct {
-	Status int             `json:"status"`
-	List   map[string]Item `json:"list"`
-}
-
-func (r RetrieveResponse) Items() []Item {
-	var items []Item
-
-	for _, item := range r.List {
-		items = append(items, item)
-	}
-
-	sort.Slice(items[:], func(i, j int) bool {
-		return items[i].AddedAt >= items[j].AddedAt
-	})
-
-	return items
-}
-
 func (c *Client) Retrieve(after string, offset int) ([]Item, error) {
 	action := "/get"
-	request := RetrieveRequest{
+	request := retrieveRequest{
 		Auth: *c.auth,
-		RetrieveOptions: RetrieveOptions{
+		retrieveOptions: retrieveOptions{
 			Since:      after,
 			Offset:     offset,
 			DetailType: "complete",
@@ -48,7 +16,7 @@ func (c *Client) Retrieve(after string, offset int) ([]Item, error) {
 			Count:      PAGE_COUNT,
 		},
 	}
-	response := RetrieveResponse{}
+	response := retrieveResponse{}
 
 	err := c.Post(action, request, &response)
 
@@ -73,4 +41,36 @@ func (c *Client) RetrieveAll(after string) ([]Item, error) {
 	}
 
 	return items, nil
+}
+
+type retrieveOptions struct {
+	DetailType string `json:"detailType"`
+	Since      string `json:"since"`
+	Offset     int    `json:"offset"`
+	Sort       string `json:"sort"`
+	Count      int    `json:"count"`
+}
+
+type retrieveRequest struct {
+	Auth
+	retrieveOptions
+}
+
+type retrieveResponse struct {
+	Status int             `json:"status"`
+	List   map[string]Item `json:"list"`
+}
+
+func (r retrieveResponse) Items() []Item {
+	var items []Item
+
+	for _, item := range r.List {
+		items = append(items, item)
+	}
+
+	sort.Slice(items[:], func(i, j int) bool {
+		return items[i].AddedAt >= items[j].AddedAt
+	})
+
+	return items
 }
